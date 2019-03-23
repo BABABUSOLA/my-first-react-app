b4f0129851a471c04e1d3a04c72204c624acc8e5
BABABUSOLA <queenbusola1@gmail.com>
2019-03-23 20:08:46 +0100

    burger app

---
+++ build/precache-manifest.a08e37392e021dc967d1377379df4bcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "41131e5a27ef8f842750",
+    "url": "/static/js/main.41131e5a.chunk.js"
+  },
+  {
+    "revision": "5d9b769d6558d0492169",
+    "url": "/static/js/2.5d9b769d.chunk.js"
+  },
+  {
+    "revision": "41131e5a27ef8f842750",
+    "url": "/static/css/main.d709523f.chunk.css"
+  },
+  {
+    "revision": "3d3c5666ce9a79fd670d287b16579ef5",
+    "url": "/index.html"
+  }
+];
